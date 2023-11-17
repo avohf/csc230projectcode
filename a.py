17ce9dfd63ca5d82aa1df43925893fdda1e60ea1
Gustavo Hernandez-Fonseca <136949179+avohf@users.noreply.github.com>
2023-11-17 11:33:23 -0500

    Update a.py
    more readable

--- a.py
+++ a.py
@@ -9,7 +9,7 @@ graph = {
 # Function to find all paths between two nodes in a graph
 def find_all_paths(graph, start, end, path=[]):
     path = path + [start]
-    #exits if the start is the end ie. one node
+    #exits if the start is the end 
     if start == end:
         return [path]
     #returns empty as there is no start
@@ -35,12 +35,25 @@ def calculate_path_length(graph, path):
 paths_to_D = find_all_paths(graph, 'A', 'D')
 paths_from_D_to_A = find_all_paths(graph, 'D', 'A')
 
-# Combine paths to find the round trips and their lengths
-round_trip_paths = [(path_to_D + path_from_D[1:], calculate_path_length(graph, path_to_D + path_from_D[1:]))
-                    for path_to_D in paths_to_D for path_from_D in paths_from_D_to_A]
+round_trip_paths = []
+for path_to_D in paths_to_D:
+    for path_from_D in paths_from_D_to_A:
+        # Combine paths while avoiding to duplicate the node D by slicing the first index of path_from_D
+        round_trip_path = path_to_D + path_from_D[1:]
+        round_trip_length = calculate_path_length(graph, round_trip_path)
+        round_trip_paths.append((round_trip_path, round_trip_length))
 
-# Find the shortest round trip path and its length
-shortest_round_trip_path, shortest_round_trip_length = min(round_trip_paths, key=lambda x: x[1])
+
+# Initialize variables to store the shortest path and its length
+# Assume the first path is the shortest initially
+shortest_round_trip_path = round_trip_paths[0][0]  # The first path
+shortest_round_trip_length = round_trip_paths[0][1]  # The length of the first path
+
+# Iterate over all round trip paths to find the one with the smallest length
+for path, length in round_trip_paths:
+    if length < shortest_round_trip_length:
+        shortest_round_trip_path = path
+        shortest_round_trip_length = length
 
 # Output the shortest path and its length
 print(f"The shortest path from A to D and back to A is {' -> '.join(shortest_round_trip_path)} with a total length of {shortest_round_trip_length}.")
